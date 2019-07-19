b3403e1d7a77bc0990fc83fb06232875d30cf46d
Aaron Klotz <aklotz@mozilla.com>
2019-07-19 15:31:26 -0600

    Error messages

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,13 @@
 #include <shtypes.h>
 
 #include <memory>
+#include <type_traits>
 
 #include <stdio.h>
+
+#pragma comment(lib, "ole32")
+#pragma comment(lib, "shell32")
+#pragma comment(lib, "wininet")
 
 static std::unique_ptr<wchar_t[]> DecodeUrl(const wchar_t* aUrl) {
   wchar_t xbuf;
@@ -16,16 +21,21 @@ static std::unique_ptr<wchar_t[]> DecodeUrl(const wchar_t* aUrl) {
   if (!::InternetCanonicalizeUrlW(aUrl, &xbuf, &bufLen,
                                   ICU_DECODE | ICU_NO_ENCODE) &&
       ::GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
+    wprintf(L"InternetCanonicalizeUrl failed with Win32 error %lu\n",
+            ::GetLastError());
     return nullptr;
   }
 
   auto buf = std::make_unique<wchar_t[]>((bufLen + 1) / sizeof(wchar_t));
   if (!buf) {
+    wprintf(L"Buffer allocation failed\n");
     return nullptr;
   }
 
   if (!::InternetCanonicalizeUrlW(aUrl, buf.get(), &bufLen,
                                   ICU_DECODE | ICU_NO_ENCODE)) {
+    wprintf(L"InternetCanonicalizeUrl failed with Win32 error %lu\n",
+            ::GetLastError());
     return nullptr;
   }
 
@@ -38,16 +48,21 @@ struct CoTaskMemFreeDeleter {
   }
 };
 
-using UniquePidl = std::unique_ptr<ITEMIDLIST, CoTaskMemFreeDeleter>;
+using UniquePidl = std::unique_ptr<std::remove_pointer<LPITEMIDLIST>::type,
+                                   CoTaskMemFreeDeleter>;
 
 int wmain(int argc, wchar_t* argv[]) {
   if (argc != 2) {
+    wprintf(L"Usage: %s <url>\n", argv[0]);
     return 1;
   }
 
   PROCESS_MITIGATION_IMAGE_LOAD_POLICY pol = {};
   pol.PreferSystem32Images = 1;
-  if (!::SetProcessMitigationPolicy(ProcessImageLoadPolicy, &pol, sizeof(pol))) {
+  if (!::SetProcessMitigationPolicy(ProcessImageLoadPolicy, &pol,
+                                    sizeof(pol))) {
+    wprintf(L"SetProcessMitigationPolicy failed with Win32 error %lu\n",
+            ::GetLastError());
     return 1;
   }
 
@@ -56,12 +71,13 @@ int wmain(int argc, wchar_t* argv[]) {
     return 1;
   }
 
-  wprintf(L"%s\n", decoded.get());
+  wprintf(L"Executing \"%s\"\n", decoded.get());
 
   LPITEMIDLIST pidl = nullptr;
   SFGAOF sfgao;
   HRESULT hr = ::SHParseDisplayName(decoded.get(), nullptr, &pidl, 0, &sfgao);
   if (FAILED(hr)) {
+    wprintf(L"SHParseDisplayName failed with HRESULT 0x%08lX\n", hr);
     return 1;
   }
 
@@ -74,6 +90,7 @@ int wmain(int argc, wchar_t* argv[]) {
   sei.lpIDList = pidl;
 
   if (!::ShellExecuteExW(&sei)) {
+    wprintf(L"ShellExecuteEx failed with Win32 error %lu\n", ::GetLastError());
     return 1;
   }
 
